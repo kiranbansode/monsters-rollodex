f52945353fa6291c0890697e7b91f04eae47ca0b
Kiran <kiran5120135@gmail.com>
2020-06-26 20:45:50 +0530

    Updates

---
+++ precache-manifest.0c67cd0e5ca3c5b98c752bb435d9671a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b84f18a8b1602b5a31f06e3b7e6b189",
+    "url": "/monsters-rollodex/index.html"
+  },
+  {
+    "revision": "c9e3aefb4ebf82d12226",
+    "url": "/monsters-rollodex/static/css/main.59d8bd4e.chunk.css"
+  },
+  {
+    "revision": "6efb027fd0c9f662f93d",
+    "url": "/monsters-rollodex/static/js/2.db2b6fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rollodex/static/js/2.db2b6fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e3aefb4ebf82d12226",
+    "url": "/monsters-rollodex/static/js/main.47c558be.chunk.js"
+  },
+  {
+    "revision": "e1138f027223282ee3fb",
+    "url": "/monsters-rollodex/static/js/runtime-main.626a21f5.js"
+  }
+]);
